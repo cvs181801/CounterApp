3514b95c9837ec39d12cc20df5a82f5727eb68c5
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-03-19 16:59:45 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+
+
+//grab button
 var myBtn = document.getElementById("btn");
 
-myBtn.addEventListener("click", function() {
+//add event listener to count with each click
+var counter = 0;
+myBtn.addEventListener("click", function(){
     counter += 1
     myBtn.textContent = counter
-})
+});
