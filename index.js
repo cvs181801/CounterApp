901ecae5450f69910a8af16405d8302d948ed1e7
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-03-19 18:10:13 -0700

    Update index.js

--- index.js
+++ index.js
@@ -10,15 +10,19 @@
 //      myBtn.textContent = counter
 //  });
 
+//fun text!
 document.body.innerHTML += "<p id='paragraph'> Hoppin' Right Along! </p>"
 document.getElementById("paragraph").style.color = "green";
 
+//grab the increment button, the decrement button, and the number
 let add = document.getElementById("incrementBtn");
 let subtract = document.getElementById("decrementBtn");
 let integer = document.getElementById("number");
 
+//set the counter to start at 0
 let counter = 0;
 
+//create an 'increment' event listener so the number will increase when button is clicked
 add.addEventListener('click', function() {
     counter += 1;
     integer.innerHTML = counter;
