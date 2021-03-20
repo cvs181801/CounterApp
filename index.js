44ec91af82929c7f42c4d2c7562d1bd2bc892380
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-03-19 17:51:31 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 
 
 //grab button
-var myBtn = document.getElementById("btn");
+// var myBtn = document.getElementById("btn");
 
 //add event listener to count with each click
-var counter = 0;
- myBtn.addEventListener("click", function(){
-     counter += 1
-     myBtn.textContent = counter
- });
+// var counter = 0;
+//  myBtn.addEventListener("click", function(){
+//      counter += 1
+//      myBtn.textContent = counter
+//  });
 
 document.body.innerHTML += "<p id='paragraph'> Hoppin' Right Along! </p>"
 document.getElementById("paragraph").style.color = "green";
