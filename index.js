b32bb9d8e28e257b8457b938e19853ae8854e4a0
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-03-19 18:04:01 -0700

    adding first click function

--- index.js
+++ index.js
@@ -12,3 +12,14 @@
 
 document.body.innerHTML += "<p id='paragraph'> Hoppin' Right Along! </p>"
 document.getElementById("paragraph").style.color = "green";
+
+let add = document.getElementById("increment");
+let subtract = document.getElementById("decrement");
+let integer = document.getElementById("number");
+
+let counter = 0;
+
+add.addEventListener('click', function()) {
+    counter += 1;
+    integer.innerHTML = counter;
+};
