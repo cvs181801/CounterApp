9eef4d7d28ea794e81f855006d76f45dc77f0b58
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-03-19 17:30:46 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,24 +2,23 @@
 
 //grab button
 var myBtn = document.getElementById("btn");
-myBtn.style.color = "lilac";
+myBtn.style.color = "yellow";
 
 //add event listener to count with each click
-// var counter = 0;
-// myBtn.addEventListener("click", function(){
-//     counter += 1
-//     myBtn.textContent = counter
-// });
+var counter = 0;
+ myBtn.addEventListener("click", function(){
+     counter += 1
+     myBtn.textContent = counter
+ });
 
-function bunnyClick() {
-    var bunny = document.getElementById("myDiv");
-    if bunny.style.display === "none") {
-        bunny.style.display = "block";
-    } else {
-        bunny.style.display = "none"
-    }
-}
-
+// function bunnyClick() {
+//     var bunny = document.getElementById("myDiv");
+//     if bunny.style.display === "none") {
+//         bunny.style.display = "block";
+//     } else {
+//         bunny.style.display = "none"
+//     }
+// }
 
 document.body.innerHTML += "<p id='paragraph'> Hoppin' Right Along! </p>"
 document.getElementById("paragraph").style.color = "green";
